d93e186c4433018025eb360e9b44e5f7f9a5e832
rhidayscrew17 <rhidayscrew@gmail.com>
2022-07-30 00:55:42 +0700

    update credwntials

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ try {
     console.error(error);
 }
 
-app.use(cors({ credentials:true, origin:'http://localhost:3000' }));
+app.use(cors({ credentials:true, origin:'http://updatenutechnodemysql.herokuapp.com' }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(router);

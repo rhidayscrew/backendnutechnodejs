32199c32c1b9102d5451177f8e80c973160b329a
rhidayscrew17 <rhidayscrew@gmail.com>
2022-08-03 15:19:38 +0700

    update acces cors

--- index.js
+++ index.js
@@ -21,21 +21,21 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(router);
 app.use(cors({ credentials:true, origin:'https://whimsical-selkie-d52dc2.netlify.app/' }));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, Accept, Accept-Version,Set-Cookie, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization"
-  );
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.header("Content-Type", "application/json");
-  res.header("Access-Control-Allow-Credentials", true);
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, Accept, Accept-Version,Set-Cookie, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+//   res.header("Content-Type", "application/json");
+//   res.header("Access-Control-Allow-Credentials", true);
 
-  if (req.method === 'OPTIONS')
-    res.status(200).send();
-  else
-    next();
-});
+//   if (req.method === 'OPTIONS')
+//     res.status(200).send();
+//   else
+//     next();
+// });
 
 const port = process.env.PORT || PORT;
 app.listen(port, ()=> console.log('Server running at port 5000'));

3bec5a6b19e9e6a113fbd22fa0479d750ed98cce
rhidayscrew17 <rhidayscrew@gmail.com>
2022-08-03 13:54:06 +0700

    update acces control and cors origin

--- index.js
+++ index.js
@@ -16,10 +16,18 @@ try {
     console.error(error);
 }
 
-app.use(cors({ credentials:true, origin:'https://updatenutechnodemysql.herokuapp.com' }));
+app.use(cors({ credentials:true, origin:'https://whimsical-selkie-d52dc2.netlify.app/' }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(router);
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Headers, *, Access-Control-Allow-Origin', 'Origin, X-Requested-with, Content_Type,Accept,Authorization','https://whimsical-selkie-d52dc2.netlify.app/');
+    if(req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
+        return res.status(200).json({});
+    }
+    next();
+});
 
 const port = process.env.PORT || PORT;
 app.listen(port, ()=> console.log('Server running at port 5000'));

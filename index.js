bb2db54ffb3b0f28248a64da4cff8a20eb62a7f9
rhidayscrew17 <rhidayscrew@gmail.com>
2022-08-03 15:16:53 +0700

    update acces cors

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(router);
 app.use(cors({ credentials:true, origin:'https://whimsical-selkie-d52dc2.netlify.app/' }));
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://whimsical-selkie-d52dc2.netlify.app/");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, Accept, Accept-Version,Set-Cookie, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization"
